bd7d39a69298a295f8e613da78ad9df46d4e9381
Kaushik Apoori <apoorikaushik@gmail.com>
2023-10-20 04:24:21 -0500

    #8

---
+++ AR tech CSCE/Assets/scripts/use.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class use : MonoBehaviour
+{
+  public void PlayGame()
+  {
+       	SceneManager.LoadScene(9);
+    
+  }
+}
